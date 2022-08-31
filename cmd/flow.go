ba11792c6d6b1d247a67db75c222547ef0a15a55
Alex Bortok <dev@bortok.com>
2022-08-30 23:40:18 -0700

    createFlow with hardcoded params

--- cmd/flow.go
+++ cmd/flow.go
@@ -24,6 +24,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/open-traffic-generator/snappi/gosnappi"
 	"github.com/spf13/cobra"
 )
 
@@ -34,7 +35,7 @@ var flowCmd = &cobra.Command{
 	Long: `Create OTG flow configuration.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("flow called")
+		createFlow()
 	},
 }
 
@@ -50,4 +51,47 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// flowCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func createFlow() {
+	// Create a new API handle to make API calls against a traffic generator
+	api := gosnappi.NewApi()
+
+	// Create a new traffic configuration that will be set on traffic generator
+	config := api.NewConfig()
+
+	// Add port locations to the configuration
+	p1 := config.Ports().Add().SetName("p1").SetLocation("${OTG_P1_LOCATION}")
+	p2 := config.Ports().Add().SetName("p2").SetLocation("${OTG_P2_LOCATION}")
+
+	// Configure the flow and set the endpoints
+	flow := config.Flows().Add().SetName("f1")
+	flow.TxRx().Port().SetTxName(p1.Name())
+	flow.TxRx().Port().SetRxName(p2.Name())
+
+	// Configure the size of a packet and the number of packets to transmit
+	flow.Size().SetFixed(128)
+	flow.Duration().FixedPackets().SetPackets(1000)
+	flow.Rate().SetPps(100)
+
+	// Configure flow metric collection
+	flow.Metrics().SetEnable(true)
+
+	// Configure the header stack
+	pkt := flow.Packet()
+	eth := pkt.Add().Ethernet()
+	ipv4 := pkt.Add().Ipv4()
+	tcp := pkt.Add().Tcp()
+
+	eth.Dst().SetValue("00:11:22:33:44:55")
+	eth.Src().SetValue("00:11:22:33:44:66")
+
+	ipv4.Src().SetValue("10.1.1.1")
+	ipv4.Dst().SetValue("20.1.1.1")
+
+	tcp.SrcPort().SetValue(5000)
+	tcp.DstPort().SetValue(6000)
+
+	// Push traffic configuration constructed so far to traffic generator
+	fmt.Println(config.ToYaml())
 }
